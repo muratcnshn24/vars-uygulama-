36520d47fad9a96b1f5d806e5eeb5fab103723d8
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 21:50:57 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vars_uygulama_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var x = 23; // Implicitly typed.
+            // int y = 23; // Explicitly typed.
+
+            string[] meyveler = { "elma", "armut", "muz", "üzüm", "şeftali" };
+
+            var meyve = from m in meyveler
+                        where m[0] == 'a'
+                        select m;
+            foreach (string m in meyve)
+            Console.WriteLine(m);
+        }
+    }
+}
